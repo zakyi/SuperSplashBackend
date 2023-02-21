b3cddd30ff055fe8e0924cb7dbd1abb922dd36c4
Yi <zhyi.feynman@gmail.com>
2023-02-22 00:09:37 +0800

    实现登录获取用户数据，并将token保存在localStorage中，实现ImageItem中使用token验证账户有效性

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var mysql = require("mysql");
+var bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -7,6 +9,8 @@ var connection = mysql.createConnection({
   password: "981213",
   database: "supersplash",
 });
+
+var secretKey = "";
 
 connection.connect((err) => {
   if (err) throw err;
@@ -16,33 +20,62 @@ connection.connect((err) => {
 var router = express.Router();
 
 /* POST users listing. */
+router.post("/verifyUser", (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+
+  connection.query("select * from privatedata", [], (err, result) => {
+    try {
+      const decode = jwt.verify(token, result[0].secretKey);
+      res.status(200).send({ message: " Token valid " });
+    } catch (err) {
+      res.status(400).send({ message: " Token invalid " });
+    }
+  });
+});
+
 router.post("/loginUser", function (req, res, next) {
   connection.query(
     `select * from users where email=?`,
     [req.body.email],
     (err, result) => {
-      if (err) {
-        res.statusCode = 400;
-        res.send({ error: "Error with sql" });
-      } else {
-        if (result.length === 0) {
-          res.statusCode = 400;
-          res.send({ error: "Email does not exist" });
-        } else {
+      if (err) res.status(500).send({ error: "Error with sql" });
+      else {
+        if (result.length === 0)
+          res.status(400).send({ error: "Email does not exist" });
+        else {
           connection.query(
             `select * from users where email=? and password=?`,
             [req.body.email, req.body.password],
             (err, result) => {
-              console.log([req.body.email, req.body.password]);
-              if (err) {
-                res.statusCode = 400;
-                res.send({ error: "Error with sql" });
-              } else {
-                if (result.length === 0) {
-                  res.statusCode = 400;
-                  res.send({ error: "Incorrect password" });
-                } else {
-                  res.send({ message: "Login success, enjoy!" });
+              if (err) res.status(400).send({ error: "Error with sql" });
+              else {
+                if (result.length === 0)
+                  res.status(400).send({ error: "Incorrect password" });
+                else {
+                  //TODO: add likes and collections
+                  const { userName, email } = result[0];
+                  connection.query(
+                    "select * from privatedata",
+                    [],
+                    (err, result) => {
+                      secretKey = result[0].secretKey;
+                      token = jwt.sign({ email, userName }, secretKey, {
+                        expiresIn: "2d",
+                      });
+                      const user = {
+                        userName,
+                        email,
+                        //TODO//
+                        likes: [],
+                        collections: [],
+                        ////////
+                        token: token,
+                      };
+                      res
+                        .status(200)
+                        .send({ message: "Login success, enjoy!", user });
+                    }
+                  );
                 }
               }
             }
@@ -54,18 +87,41 @@ router.post("/loginUser", function (req, res, next) {
 });
 
 router.post("/addUser", function (req, res, next) {
-  console.log("add user");
-  console.log([req.body.email, req.body.password, req.body.userName]);
   connection.query(
-    `insert into users (email, password, userName) values (?, ?, ?)`,
-    [req.body.email, req.body.password, req.body.userName],
+    "select * from users where email=?",
+    [req.body.email.toLowerCase()],
     (err, result) => {
-      if (err && err.code === "ER_DUP_ENTRY") {
-        // res.set({ "Content-Type": "text/plain" });
-        res.statusCode = 400;
-        res.send({ error: "Email already exists" });
+      if (err) {
+        return res.status(500).send("Server error");
+      } else if (result.length > 0) {
+        return res.status(409).send("Email already exists");
       } else {
-        res.send({ message: "Register success, enjoy!" });
+        connection.query("select * from privatedata", [], (err, result) => {
+          const { secretKey } = result[0];
+          token = jwt.sign(
+            { email: req.body.email, userName: req.body.userName },
+            secretKey,
+            {
+              expiresIn: "2d",
+            }
+          );
+          connection.query(
+            `insert into users (email, password, userName, token) values (?, ?, ?, ?)`,
+            [
+              req.body.email.toLowerCase(),
+              req.body.password,
+              req.body.userName,
+              token,
+            ],
+            (err, result) => {
+              if (err) {
+                res.status(500).send("Server error");
+              } else {
+                res.send({ message: "Register success, enjoy!" });
+              }
+            }
+          );
+        });
       }
     }
   );
